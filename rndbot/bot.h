89a184eeff7287762d780b6d571910d71b967d86
yfzcsc <35807099+yfzcsc@users.noreply.github.com>
2020-12-02 17:09:17 +0800

    Create bot.h

---
+++ rndbot/bot.h
@@ -0,0 +1,9 @@
+#ifndef __BOT_H__
+#define __BOT_H__
+#include<bits/stdc++.h>
+const int TILE_EMPTY=-1;
+const int TILE_MOUNTAIN=-2;
+const int TILE_FOG=-3;
+const int TILE_FOG_OBSTACLE=-4;
+const int N=500;
+#endif
